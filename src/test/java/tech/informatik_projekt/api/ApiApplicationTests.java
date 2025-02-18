73f434a7db4a5ebd37f2ac16449147d716393dee
Nils <contact@piste.dev>
2025-02-18 10:43:45 +0100

    initial commit

---
+++ src/test/java/tech/informatik_projekt/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.informatik_projekt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
