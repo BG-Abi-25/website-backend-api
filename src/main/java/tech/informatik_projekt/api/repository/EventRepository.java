cbcb5ff017843d5da41274f7e83c5f0d07ebf67b
Nils <contact@piste.dev>
2025-03-05 13:29:56 +0100

    fix: added MongoRepository extension

--- src/main/java/tech/informatik_projekt/api/repository/EventRepository.java
+++ src/main/java/tech/informatik_projekt/api/repository/EventRepository.java
@@ -1,12 +1,14 @@
 package tech.informatik_projekt.api.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import tech.informatik_projekt.api.model.BlogPost;
 import tech.informatik_projekt.api.model.Event;
 
 import java.util.List;
 
 @Repository
-public interface EventRepository {
+public interface EventRepository extends MongoRepository<Event, Integer> {
     List<Event> findAll();
     Event findById(int id);
 }
