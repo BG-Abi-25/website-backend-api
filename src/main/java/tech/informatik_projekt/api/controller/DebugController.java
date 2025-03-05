d3b93cd7ad3312b46e8182818273545a1708a626
Nils <contact@piste.dev>
2025-03-05 12:58:54 +0100

    fix: added RestController annotation

--- src/main/java/tech/informatik_projekt/api/controller/DebugController.java
+++ src/main/java/tech/informatik_projekt/api/controller/DebugController.java
@@ -2,10 +2,12 @@ package tech.informatik_projekt.api.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author <a href="https://github.com/zpiste">Piste</a>
  */
+@RestController
 public class DebugController {
 
     @Value("${MONGO_URI:NOT_SET}")
