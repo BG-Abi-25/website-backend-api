59ddc43ec7539315ac65d2a1cd6d32fd5f6599b1
Nils <contact@piste.dev>
2025-03-05 12:58:25 +0100

    fix: debug env variables

---
+++ src/main/java/tech/informatik_projekt/api/controller/DebugController.java
@@ -0,0 +1,20 @@
+package tech.informatik_projekt.api.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author <a href="https://github.com/zpiste">Piste</a>
+ */
+public class DebugController {
+
+    @Value("${MONGO_URI:NOT_SET}")
+    private String mongoUri;
+
+    @GetMapping("/v1/env")
+    public String checkEnv() {
+        System.out.println("MONGO_URI: " + mongoUri);
+        return "MONGO_URI: " + mongoUri;
+    }
+
+}
